e69736ca06e6bab704ba4232bc7b45bd7ec2bf43
liyanqwq <liyanqwq@duianit.cn>
2022-03-10 21:49:33 +0800

    Initialization

---
+++ README.md
@@ -0,0 +1,32 @@
+# HydroOJ Countdown
+
+适用于 [HydroOJ](//hydro.js.org) 的倒计时插件。
+
+## Installation
+
+克隆本项目，执行 `yarn`，添加到 Hydro 插件。
+
+进入 HydroOJ 控制面板 - hydrooj - 主页，将下方配置信息粘贴到适当位置。
+
+```
+  countdown:
+    calendar_url: "https://www.noi.cn/upload/resources/image/2022/01/11/183119.jpg"
+    max_dates: 3
+    dates:
+      - name: NOI Online 2022
+        date: 2022-03-26
+      - name: NOI 2022 省选 / NGOI
+        date: 2022-04-04
+      - name: NOI 2022
+        date: 2022-07-23
+      - name: IOI 2022
+        date: 2022-08-07
+      - name: CSP 2022 一轮
+        date: 2022-09-18
+      - name: CSP 2022 二轮
+        date: 2022-10-22
+      - name: NOIP 2022
+        date: 2022-11-19
+```
+
+然后应该就能用了，其他见上面配置。
---
+++ handler.js
@@ -0,0 +1,39 @@
+const { Route, Handler } = global.Hydro.service.server;
+const { PRIV } = global.Hydro.model.builtin;
+const pastebin = global.Hydro.model.pastebin;
+const system = global.Hydro.model.system;
+const { checkContent } = global.Hydro.lib.validator;
+const { NotFoundError } = global.Hydro.error;
+const { PermissionError } = global.Hydro.error;
+const yaml = require('js-yaml');
+const moment = require('moment');
+class CountdownGetDateHandler extends Handler {
+    async get() {
+        var hdoc = yaml.load(system.get('hydrooj.homepage'));
+        var content = new Array();
+        var dateToday = moment().format("YYYY-MM-DD");
+        var dates = new Array(hdoc[1]['countdown']['dates']);
+        dates = dates[0];
+        console.log(dates);
+        dates.forEach(function(val,ind){
+            if(content.length < hdoc[1]['countdown']['max_dates']){
+                if(moment(val.date).isSameOrAfter(dateToday)){
+                    // console.log(val);
+                    var diffTime = moment(val.date).diff(moment(), 'days');
+                    // console.log(diffTime);
+                    content.push({
+                        name: val.name,
+                        diff: diffTime
+                    })
+                }
+            }
+        });
+        this.response.body = content;
+    }
+}
+
+async function apply() {
+    Route('countdown_getdate', '/countdown/getdate', CountdownGetDateHandler);
+}
+
+global.Hydro.handler.countdown = apply;
